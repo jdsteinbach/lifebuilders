7bac9ccd5bf15e42c1e65cac29a03ec8356d886b
James Steinbach <jdsteinbach@gmail.com>
2022-10-21 11:09:03 -0600

    misc tweaks

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,6 @@
 module.exports = {
-  site_title: 'LifeBuilders',
+  site_title: 'Lessons & Sermons',
+  site_author: 'James Steinbach',
   base_url: '/',
   first_year: 2013,
   current_year: new Date().getFullYear()
